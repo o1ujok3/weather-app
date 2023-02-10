6e140fed0e4ac66195c15e51e8ef2e23598b5abd
Olujoke Fakoya <113132389+o1ujok3@users.noreply.github.com>
2023-02-10 10:14:38 +0000

    js code finalised

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,30 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-var currentTime = moment().format("(DD/MM/YYYY)");
+//Declaring global variables
 var apiKey = "993fe4dc40ea497120aa35240de660b9";
-var city = "London";
 var baseURL = "https://api.openweathermap.org/data/2.5/";
-var currentURL = baseURL + `weather?appid=${apiKey}&units=metric&`;
-var forecastURL = baseURL + `forecast?appid=${apiKey}&units=metric&`;
-var iconURL = "https://openweathermap.org/img/w/";
+var currentURL = baseURL + `weather?&appid=${apiKey}&units=metric&`;
+var forecastURL = baseURL + `forecast?&appid=${apiKey}&units=metric&`;
+var currentTime = moment().format("(DD/MM/YYYY)");
 
-function inputSubmitted(cityName) {
-  $.get(currentURL + `q=${cityName}`).then(function (currentData) {
-    console.log(currentData);
+// Populating elements from current and 5 day forecast
+function populateElements(obj) {
+  var mainObj = obj.main;
 
-    console.log(`
-    Temp: ${Math.round(currentData.main.temp)}
-    Humidity: ${currentData.main.humidity}%
-    Wind: ${currentData.wind.speed}
-    IconURL: ${iconURL + currentData.weather[0].icon}.png
-    `);
+  var windSpd = obj.wind.speed * 3.6;
 
-    $.get(
-      forecastURL + `lat=${currentData.coord.lat}&lon=${currentData.coord.lon}`
-    ).then(function (forecastData) {
-      //   console.log(forecastData);
-      for (var castObj of forecastData.list) {
-        console.log(`${iconURL + castObj.weather[0].icon}.png`);
-      }
-    });
-  });
+  return `
+    <p>Temp: ${mainObj.temp}&#8451;</p>
+    <p>Wind: ${windSpd.toFixed(2)} KPH</p>
+    <p>Humidity: ${mainObj.humidity} %</p>
+    `;
 }
 
-// Function to return icon image for weather
-function outputImg(data) {
+//Return icon for weather for specific day
+function outputIcon(data) {
   return `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}"></img>`;
 }
 
-// Function for getting 5 day forecast data
-function getForecastWeather(lon, lat) {
+//Get 5-day forecast data
+function fetch5DayForecast(lon, lat) {
   $.get(forecastURL + `lat=${lat}&lon=${lon}`).then(function (data) {
     $("#forecast").removeClass("invisible");
 
@@ -52,22 +39,22 @@ function getForecastWeather(lon, lat) {
     for (var index in forecastArr) {
       var dateTxt = forecastArr[index].dt_txt;
 
-      var isNoon = dateTxt.includes("12:");
+      var isMidday = dateTxt.includes("12:");
 
-      if (isNoon) {
+      if (isMidday) {
         var futureDate = moment().add(count, "days").format("DD/MM/YYYY");
 
         var currentItem = forecastArr[index];
 
         cardContainer.append(`
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="mt-1 h5">${futureDate}</h5>
-                  ${outputImg(currentItem)}
-                  ${populateEls(currentItem)}
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="mt-1 h5">${futureDate}</h5>
+                    ${outputIcon(currentItem)}
+                    ${populateElements(currentItem)}
+                  </div>
                 </div>
-              </div>
-              `);
+                `);
 
         count++;
       }
@@ -75,21 +62,73 @@ function getForecastWeather(lon, lat) {
   });
 }
 
-/*
-When page loads: 
+//Get data for current date
+function fetchCurrentWeather(search) {
+  $.get(currentURL + `q=${search}`).then(function (data) {
+    $("#today")
+      .html(
+        `
+        <h3 class="mt-1 h3">${data.name} ${currentTime} ${outputIcon(data)}</h3>
+        ${populateElements(data)}
+        `
+      )
+      .removeClass("invisible");
 
-1. Show user an input to allow them to search for a city
-    •Show a message on the page to point them, or guide them, to the input
-    -Once city has been inputted:
-    a) Show current forecast
-    b) show 5 day forecast
-    c) add city to search history
-        - Get previous searches from localStorage
-        - If inoutted city has not been stored to search history, push the city name
-        - Set the search history to localStorage
-2. Show search history
-    -Pull search history from localStorage
-    -If search history is not empty, output each city to the search history display in the DOM
+    var coord = data.coord;
 
-*/
-//function getting data for current date
+    fetch5DayForecast(coord.lon, coord.lat);
+  });
+}
+
+//When search button is clicked
+function searchBtnClicked(event) {
+  event.preventDefault();
+
+  var searchInput = $("#search-input");
+
+  var searchVal = searchInput.val().toLowerCase();
+
+  if (searchVal) {
+    fetchCurrentWeather(searchVal);
+
+    var returnedLocalStorage = getLocalStorage();
+
+    returnedLocalStorage[searchVal] = searchVal;
+
+    localStorage.setItem(
+      "weather-history",
+      JSON.stringify(returnedLocalStorage)
+    );
+
+    searchInput.val("");
+  }
+}
+
+//Return search history from local storage
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("weather-history")) || {};
+}
+
+function init() {
+  var historyElement = $("#history");
+
+  historyElement.html("");
+
+  var history = getLocalStorage();
+
+  if (history) {
+    for (var item in history) {
+      historyElement.append(`
+            <button class="historyBtn btn btn-secondary m-1">${history[item]}</button>
+            `);
+    }
+  }
+
+  $(".historyBtn").on("click", function (event) {
+    fetchCurrentWeather(event.target.innerHTML);
+  });
+
+  $("#search-button").on("click", searchBtnClicked);
+}
+
+init();
